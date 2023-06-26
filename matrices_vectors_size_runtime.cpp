8dc926c80c568cacab356a7f151ed9881e52dc29
SodaCris <wjxa20152015@gmail.com>
2023-06-26 19:02:09 +0800

    two test code

---
+++ matrices_vectors_size_runtime.cpp
@@ -0,0 +1,20 @@
+#include "boost/format/format_fwd.hpp"
+#include <Eigen/Dense>
+#include <boost/format.hpp>
+#include <iostream>
+#include <sstream>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+int main()
+{
+    MatrixXd m = MatrixXd::Random(3, 3);
+    m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
+    std::cout << "m =" << std::endl
+              << m << std::endl;
+    VectorXd v(3);
+    v << 1, 2, 3;
+    std::cout << "m * v =" << std::endl
+              << m * v << std::endl;
+}
---
+++ test_compile_error.cpp
@@ -0,0 +1,9 @@
+#include <Eigen/Dense>
+using namespace Eigen;
+
+int main()
+{
+    Matrix3f m;
+    Vector4f v;
+    v = m * v;
+}
